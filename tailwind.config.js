dfd1e6e7e75716aab2bc5a98fa664dc58fd2ef70
Abdulateef Abdulsalam <abdulsalamopeyemi030@gmail.com>
2025-01-22 10:31:27 +0100

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*index.html"],
   theme: {
     screens: {
       sm: "480px",
@@ -22,4 +22,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
